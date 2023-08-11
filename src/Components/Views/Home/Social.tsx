01adee428c322175c03ec3a21fb8f65d9fbd91f0
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-08-11 21:49:01 +0500

    commit

--- src/Components/Views/Home/Social.tsx
+++ src/Components/Views/Home/Social.tsx
@@ -7,8 +7,8 @@ const Social = () => {
     return (
         <section className='social-bg py-10'>
             <Container>
-                <div className='flex justify-between py-10'>
-                    <h2 className='text-5xl font-bold text-primary'>Get Social With Us</h2>
+                <div className='flex flex-wrap justify-between py-10'>
+                    <h2 className='text-5xl font-bold text-primary pb-5 sm:pb-0'>Get Social With Us</h2>
                     <Button text={'Explore Our Instagram'} display={''} />
                 </div>
                 <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 py-10'>
--- src/Components/Views/Home/Ultimate.tsx
+++ src/Components/Views/Home/Ultimate.tsx
@@ -6,13 +6,13 @@ const Ultimate = () => {
     return (
         <Container>
             <section className='relative mb-20'>
-                <Image className='w-full max-h-[500px] object-cover' src={'/girl.jpg'} alt={''} width={1000} height={500} />
+                <Image className='w-full sm:max-h-[500px] object-cover' src={'/girl.jpg'} alt={''} width={1000} height={500} />
                 <div className='absolute inset-0 flex items-center justify-center'>
                     <div className='w-full h-full bg-black opacity-20 shadow-inner' />
                 </div>
-                <div className=' absolute text-white  bottom-10 z-[1000] px-6 '>
-                    <h2 className=' text-5xl font-semibold pb-2'>The ultimate family travel guide</h2>
-                    <p className='font-medium text-xl pb-3'>An inspo packaged for getting your next trip into gear</p>
+                <div className='absolute text-white  bottom-10 z-[1000] px-6 '>
+                    <h2 className='text-3xl sm:text-5xl font-semibold pb-2'>The ultimate family travel guide</h2>
+                    <p className='font-medium sm:text-xl pb-3'>An inspo packaged for getting your next trip into gear</p>
                     <button className='bg-white text-black px-4 py-1.5 rounded-full'>
                         Read More
                     </button>
